9a10aa88aa245bd3e0a996f327224e9ec23542e7
youngandmad <kkraken2005@gmail.com>
2024-02-02 00:49:34 +0600

    auth service

--- src/app/features/auth/services/auth.service.ts
+++ src/app/features/auth/services/auth.service.ts
@@ -1,9 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RegistrationDto } from '../model/registration.dto';
+import { Observable } from 'rxjs';
+import { LoginDto } from '../model/login.dto';
+import { TokenDto } from '../model/token.dto';
+import { ConfirmEmailDto } from '../model/confirm-email.dto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private readonly _http: HttpClient) {}
+
+  register = (dto: RegistrationDto): Observable<boolean> =>
+    this._http.post<boolean>('/api/v1/user/register', dto);
+
+  login = (dto: LoginDto): Observable<TokenDto> =>
+    this._http.post<TokenDto>('/api/v1/user/login', dto);
+
+  confirmEmail = (dto: ConfirmEmailDto): Observable<TokenDto> =>
+    this._http.post<TokenDto>('/api/v1/user/comfirm-email', dto);
+
+  refreshToken = (token: string): Observable<TokenDto> =>
+    this._http.post<TokenDto>(
+      '/api/v1/user/refresh-token',
+      {},
+      {
+        headers: {
+          'refresh-token': token,
+        },
+      }
+    );
 }
