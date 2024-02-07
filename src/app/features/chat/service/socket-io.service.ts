804230fa3d745f29f03d57a2832c22fe73e5cea5
youngandmad <kkraken2005@gmail.com>
2024-02-08 01:52:38 +0600

    socket io service init

--- src/app/features/chat/service/socket-io.service.ts
+++ src/app/features/chat/service/socket-io.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-import * as io from 'socket.io-client';
+import { BehaviorSubject } from 'rxjs';
+import { io, Socket } from 'socket.io-client';
+import { EnvService } from 'src/app/common/service/env.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SocketIoService {
-  constructor() {}
+  private socket: Socket;
+  public message$: BehaviorSubject<string> = new BehaviorSubject('');
+
+  constructor(private readonly _envService: EnvService) {
+    this.socket = io(this._envService.socketUrl);
+  }
+
+  sendMessage(message: any) {
+    console.log('sendMessage: ', message);
+    this.socket.emit('message', message);
+  }
+
+  getNewMessage = () => {
+    this.socket.on('message', (message) => {
+      this.message$.next(message);
+    });
+
+    return this.message$.asObservable();
+  };
 }
