2754c81913555e780a5d0b5bb764b9d881dfb561
youngandmad <kkraken2005@gmail.com>
2024-02-11 02:08:43 +0600

    chat service

---
+++ src/app/features/chat/service/chat.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ChatService {
+  constructor(private readonly _http: HttpClient) {}
+
+  delete = (id: number): Observable<any> =>
+    this._http.delete(`/api/v1/chat/${id}`);
+
+  create = (senderId: number, receiverId: number): Observable<{ id: number }> =>
+    this._http.post<{ id: number }>('/api/v1/chat', { senderId, receiverId });
+}
